af1a011fb0e32e684bd5ea115e271357428939d5
Khang <khangnguyenhoangbao@gmail.com>
2024-11-19 19:36:01 +0700

    game loop

--- src/main/Game.java
+++ src/main/Game.java
@@ -2,29 +2,33 @@ package main;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
 import static javax.imageio.ImageIO.read;
 
-public class Game extends JFrame {
+public class Game extends JFrame implements Runnable{
 
     private GameScreen gameScreen;
-
     private BufferedImage img;
+    private Thread gameThread;
+
+    private final double FPS_SET = 120.0;
+    private final double UPS_SET = 60.0;
 
     public Game() {
 
         importImg();
 
         setSize(640,640);
-        setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
         gameScreen = new GameScreen(img);
         add(gameScreen);
+        setVisible(true);
     }
 
     private void importImg() {
@@ -38,8 +42,60 @@ public class Game extends JFrame {
         }
     }
 
+    private void start(){
+
+        gameThread = new Thread(this){};
+        gameThread.start();
+    }
+
+    private void updateGame() {
+
+        //System.out.println("Game Updated!");
+    }
+
     public static void main(String[] args) {
 
         Game game = new Game();
+        game.start();
+
+    }
+
+    @Override
+    public void run() {
+
+        double timePerFrame = 1000000000.0 / FPS_SET;
+        double timePerUpdate = 1000000000.0 / UPS_SET;
+
+        long lastFrame = System.nanoTime();
+        long lastUpdate =System.nanoTime();
+        long lastTimeCheck = System.currentTimeMillis();
+
+        int frames = 0;
+        int updates = 0;
+
+        while (true){
+
+            //Render
+            if(System.nanoTime() - lastFrame >= timePerFrame){
+                repaint();
+                lastFrame = System.nanoTime();
+                frames++;
+            }
+
+            //Update
+            if(System.nanoTime() - lastUpdate > timePerUpdate) {
+                updateGame();
+                lastUpdate = System.nanoTime();
+                updates++;
+            }
+
+            //checking FPS and UPS
+            if(System.currentTimeMillis() - lastTimeCheck >= 1000){
+                System.out.println("FPS: " + frames + "| UPS: " + updates);
+                frames = 0;
+                updates = 0;
+                lastTimeCheck = System.currentTimeMillis();
+            }
+        }
     }
 }
--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -21,6 +21,9 @@ public class GameScreen extends JPanel {
         loadSprites();
 
         random = new Random();
+
+
+
     }
 
     private void loadSprites() {
